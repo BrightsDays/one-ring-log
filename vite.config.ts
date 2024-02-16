3c6f87e61805980df1f133e7b2ac447534ab6d02
vivanov <brightsdayss@gmail.com>
2024-02-16 16:49:08 +0400

    set deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
+    base: 'one-ring-log',
     plugins: [react()],
     define: {
       'process.env': JSON.stringify(env)

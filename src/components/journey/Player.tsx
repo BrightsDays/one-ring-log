00c6e97a74bae056905f8c4bec7376aaee1ddaa1
vivanov <brightsdayss@gmail.com>
2024-03-14 12:04:51 +0400

    add smartphone view

--- src/components/journey/Player.tsx
+++ src/components/journey/Player.tsx
@@ -40,7 +40,7 @@ export const Player = ({player, editable, playerEvent}: Props) => {
   useEffect(() => { updateData('fatigue', fatigue) }, [fatigue])
 
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="grid sm:grid-cols-3 grid-cols-2 gap-2">
       <TextInput disabled={!editable} value={name || ''} inputEvent={(value) => setName(value)}>
         <DeleteRowButton
           show={(!name.length && editable) ? true : false}
